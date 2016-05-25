416b2a26a29f5bdb1330fa65e0da19067e36a874
Jason Burns <admiralmudkip@gmail.com>
2016-05-25 01:10:36 -0700

    movement works

---
+++ Assets/SPINFUSOR.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SPINFUSOR : MonoBehaviour {
+
+    public GameObject disc;
+    Rigidbody discBody;
+    private int speed = 70;
+
+    void Start()
+    {
+        discBody = disc.GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            //GameObject firedDisc = (GameObject)Instantiate(disc, transform.parent.rotation.eulerAngles, new Quaternion());
+            //firedDisc.transform.position = 
+        }
+
+	}
+}
