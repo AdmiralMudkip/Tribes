276b6ede1ce4eb8d2cb000e895c134e457aa2cc8
Jason Burns <admiralmudkip@gmail.com>
2016-06-28 23:28:41 -0700

    All commit messages, including the last one are from whatthecommit.

--- Assets/WeaponManager.cs
+++ Assets/WeaponManager.cs
@@ -10,62 +10,95 @@ public class WeaponManager : MonoBehaviour {
     public GameObject weaponPlacement;
     public int activeWeapon = 0;
 
-    public string[] weaponDirectories = { "Weapons/Spinfusor", "Weapons/Rifle" };
+    public string[] weaponDirectories = { "SpinfusorPrefab", "RiflePrefab" };
 
     void Start()
     {
         //weaponPlacement.
         weapons = new Weapon[5];
-        weapons[0] = new Spinfusor(projectilePrefabs[0], barrelEnd);
+        weapons[0] = new Spinfusor(projectilePrefabs[0], barrelEnd, 2);
+        weapons[1] = new Rifle(projectilePrefabs[1], barrelEnd, .1f);
         loadWeapon(0);
     }
-
-
-
-
+    
     // Update is called once per frame
 	void Update () {
-	    if(Input.GetButtonDown("Fire1"))
+        // dakka
+	    if(Input.GetButton("Fire1") && Weapon.ready)
         {
             weapons[activeWeapon].fire();
+            Invoke("readyWeapon", weapons[activeWeapon].cycleSpeed);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha0))
+        weaponSwitch();
+	}
+
+    void readyWeapon()
+    {
+        Weapon.setReady();
+    }
+
+    void weaponSwitch()
+    {
+        if (Input.GetButtonDown("1"))
         {
             activeWeapon = 1;
-        } 
-        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        }
+        else if (Input.GetButtonDown("2"))
         {
             activeWeapon = 2;
         }
-	}
+        else if (Input.GetButtonDown("3"))
+        {
+            activeWeapon = 3;
+        }
+        else if (Input.GetButtonDown("4"))
+        {
+            activeWeapon = 4;
+        }
+        else if (Input.GetButtonDown("5"))
+        {
+            activeWeapon = 5;
+        }
+
+        loadWeapon(activeWeapon);
+    }
 
     void loadWeapon(int num)
     {
         Mesh m = Resources.Load(weaponDirectories[num], typeof(Mesh)) as Mesh;
-        weaponPlacement.GetComponent<MeshFilter>().mesh = m;
+        weaponPlacement.GetComponent<MeshFilter>().mesh = Resources.Load(weaponDirectories[num], typeof(Mesh)) as Mesh;
     }
     
+    
+
     public abstract class Weapon
     {
         public Rigidbody projectilePrefab;
         public Transform barrelEnd;
         public int velocity;
+        public static bool ready = true;
+        public float cycleSpeed;
         // generic shooting, inherited classes will overwrite
-        // reload? 
         
         public abstract void fire();
+        public static void setReady()
+        {
+            ready = true;
+        }
     }
 
     public abstract class SingleShotWeapon : Weapon
     {
-        public bool hasFired;
-        int cycleSpeed;
-
+        public override void fire()
+        {
+            ready = false;
+            
+        }
     }
 
     public abstract class MultiFireWeapon : Weapon
     {
-        int RPM;
+
     }
 
 
@@ -73,15 +106,18 @@ public class WeaponManager : MonoBehaviour {
     {
         int torque = 500;
         
-        public Spinfusor(Rigidbody prefab, Transform barrel)
+        public Spinfusor(Rigidbody prefab, Transform barrel, float cycleSpeed)
         {
             barrelEnd = barrel;
             projectilePrefab = prefab;
             velocity = 2000;
+            this.cycleSpeed = cycleSpeed;
         }
         
         public override void fire()
         {
+            base.fire();
+            
             Rigidbody discInstance;
             discInstance = Instantiate(projectilePrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
             discInstance.AddForce(barrelEnd.up * -1 * velocity);
@@ -89,11 +125,14 @@ public class WeaponManager : MonoBehaviour {
         }
     }
 
-    class Rifle : Weapon
+    class Rifle : SingleShotWeapon
     {
-        public Rifle()
+        public Rifle(Rigidbody prefab, Transform barrel, float cycleSpeed)
         {
+            barrelEnd = barrel;
+            projectilePrefab = prefab;
             velocity = 6000;
+            this.cycleSpeed = cycleSpeed;
         }
 
         public override void fire()
